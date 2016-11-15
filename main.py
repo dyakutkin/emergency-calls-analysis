a4799f426048be3f98d812f8c88549825833b6ab
Dmitry Yakutkin <dmitry.yakutkin@gmail.com>
2016-11-15 15:36:35 +0200

    Hours aggregation rough implementation

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import csv
 import settings
 
+import requests
+
 from dateutil.parser import parse as parse_datetime
+from pprint import pprint
+from collections import defaultdict, OrderedDict
 
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 from elasticsearch.exceptions import NotFoundError
 
-es = Elasticsearch()
+es = Elasticsearch(hosts=[{'host': settings.ELASTICSEARCH_HOSTNAME, 'port': settings.ELASTICSEARCH_PORT}])
 
 INDEX_NAME = 'calls'
 
@@ -15,14 +19,16 @@ INDEX_NAME = 'calls'
 def get_actions(filename=settings.DATASET_FILENAME):
     with open(filename) as csv_file:
         reader = csv.DictReader(csv_file)
-        for row in reader:
-            row['timestamp'] = parse_datetime(row.pop('timeStamp'))
-            yield {
-                '_op_type': 'create',
+        for index, row in enumerate(reader):
+            row['date'] = parse_datetime(row.pop('timeStamp')).strftime("%Y-%m-%dT%H:%M")
+            data = {
+                '_op_type': 'index',
                 '_index': INDEX_NAME,
                 '_type': 'call',
-                'body': row
+                '_id': index,
             }
+            data.update(row)
+            yield data
 
 
 def initialize_data(filename=settings.DATASET_FILENAME):
@@ -36,13 +42,25 @@ def initialize_data(filename=settings.DATASET_FILENAME):
 
 if __name__ == '__main__':
     initialize_data()
-    print(es.search(index=INDEX_NAME, doc_type='document', body={
+    get_actions(settings.DATASET_FILENAME)
+    r = requests.get('http://elasticsearch:9200/calls/_search', json={
         "aggregations": {
-            "simpleDatehHistogram": {
+            "simpleDateHistogram": {
                 "date_histogram": {
-                    "field": "timestamp",
-                    "interval": "minute",
+                    "field": "date",
+                    "interval": "hour",
                 }
             }
         }
-    }))
+    })
+    results = defaultdict(int)
+    items = r.json().get('aggregations').get('simpleDateHistogram').get('buckets')
+    print(items[0])
+    for item in items:
+        time = parse_datetime(item.get('key_as_string'))
+        results[time.hour] += item.get('doc_count')
+
+    ordered_results = OrderedDict()
+    for key in sorted(results, key=lambda key: results[key], reverse=True):
+        ordered_results[key] = results[key]
+    pprint(ordered_results)
